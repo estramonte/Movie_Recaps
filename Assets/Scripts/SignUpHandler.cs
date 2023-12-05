975574f0b9929dbe6e416f1a81969698791ae225
estramonte <estramonte@chapman.edu>
2023-12-05 15:24:29 -0800

    Fixed Issues

--- Assets/Scripts/SignUpHandler.cs
+++ Assets/Scripts/SignUpHandler.cs
@@ -7,8 +7,7 @@ using TMPro; // Add this line at the top of your script
 
 public class SignUpHandler : MonoBehaviour
 {
-    public TMP_InputField FirstNameInputField;
-    public TMP_InputField LastNameInputField;
+    public TMP_InputField EmailInputField;
     public TMP_InputField DOBInputField;
     public TMP_InputField usernameInputField;
     public TMP_InputField passwordInputField;
@@ -16,11 +15,13 @@ public class SignUpHandler : MonoBehaviour
     public void onSignUpSubmitButtonClicked()
     {
         // Store the username and password values from the input fields.
-        string firstName = FirstNameInputField.text;
-        string lastName = LastNameInputField.text;
+        string email = EmailInputField.text;
         string DOB = DOBInputField.text; // maybe change to Date later
         string username = usernameInputField.text;
         string password = passwordInputField.text;
+        
+        // sign user up
+        // send email, DOB, username, and password to http thing
 
         // load main menu screen
         SceneManager.LoadScene("Main Menu");
